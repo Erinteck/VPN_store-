09bbe25f92d1dbd6e2c2da83bec3d204650bb258
Erinteck <erinteck@gmail.com>
2025-08-02 07:54:30 -0700

    Update database.py

--- project/database.py
+++ project/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, create_engine
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, create_engine ,BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
@@ -16,12 +16,17 @@ class Payment(Base):
     family_info = Column(String)
     receipt_path = Column(String)
     is_confirmed = Column(Boolean, default=False)
+    confirmed_by = Column(String, nullable=True)
+    rejected_by = Column(String, nullable=True)
+    status = Column(String, default="pending")  
+    handled_by = Column(BigInteger)  
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     reminder_sent = Column(Boolean, default=False)
+    
 
 class QRCode(Base):
     __tablename__ = "qrcodes"
-    id = Column(Integer, primary_key=True)  # 1 تا 100
+    id = Column(Integer, primary_key=True)  
     filename = Column(String)
     is_used = Column(Boolean, default=False)
 
